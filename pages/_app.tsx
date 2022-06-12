34432de0182ccddb93699a5fa7a1cda5eea7c3dc
Adam Kula <adamkula97@gmail.com>
2022-06-12 12:45:11 +0200

    add usestate all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,35 +2,6 @@ import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 
-
-type Planets = {
-  name: string;
-  overview: {
-    content:string;
-    source: string;
-  }
-  structure: {
-    content:string;
-    source: string;
-  }
-  geology: {
-    content:string;
-    source: string;
-  }
-  rotation: string;
-  revolution: string;
-  radius: string;
-  temperature: string;
-  images: {
-    planet: string;
-    internal: string;
-    geology: string;
-  }
-};
-
-
-
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
